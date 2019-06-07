351aa7f49a17f20752353fc25047e41479cb4575
Liam O'Boyle <liam@elyobo.net>
2019-06-07 23:28:59 +1000

    Wire up some basic cleanliness.
    Not totally a fan of it (no trailing commas) but is otherwise close to
    my standard anyway.

--- src/run.js
+++ src/run.js
@@ -1,22 +1,20 @@
 'use strict'
 
 const { pass, fail } = require('create-jest-runner')
-const { default: toTestResult } = require('create-jest-runner/build/toTestResult')
-const validator = require('oas-validator') 
-const linter = require('oas-linter') 
+const {
+  default: toTestResult
+} = require('create-jest-runner/build/toTestResult')
+const validator = require('oas-validator')
+const linter = require('oas-linter')
 
-const warningToTest = (duration, testPath) => (warning) => {
-  const {
-    ruleName,
-    message,
-    pointer,
-  } = warning
+const warningToTest = (duration, testPath) => warning => {
+  const { ruleName, message, pointer } = warning
 
   return {
     duration,
     errorMessage: message, // Does not seem to be used in reporters
     testPath,
-    title: `${message} - ${pointer} (${ruleName})`,
+    title: `${message} - ${pointer} (${ruleName})`
   }
 }
 
@@ -25,11 +23,11 @@ const run = ({ testPath, config, globalConfig }) => {
   const schema = require(testPath)
   const test = {
     path: testPath,
-    title: 'OAS Linter',
+    title: 'OAS Linter'
   }
   const result = {
     start: Date.now(),
-    test,
+    test
   }
 
   if (!schema || typeof schema !== 'object') {
@@ -39,7 +37,7 @@ const run = ({ testPath, config, globalConfig }) => {
       test: {
         ...test,
         errorMessage: `${testPath} does not resolve to an object, cannot be a valid schema`
-      },
+      }
     })
   }
 
@@ -52,54 +50,62 @@ const run = ({ testPath, config, globalConfig }) => {
         object: 'schema',
         description: 'schema properties should have an example',
         truthy: 'description'
-      },
-    ],
+      }
+    ]
   })
   /**/
-  
+
   return new Promise((resolve, reject) => {
-    validator.validate(schema, {
-      skip: false,
-      lint: true,
-      linter: linter.lint,
-      linterResults: linter.getResults,
-      prettify: true,
-      verbose: false,
-    }, (error, options) => {
-      const { context, warnings, valid } = options || error.options
-      result.end = Date.now()
+    validator.validate(
+      schema,
+      {
+        skip: false,
+        lint: true,
+        linter: linter.lint,
+        linterResults: linter.getResults,
+        prettify: true,
+        verbose: false
+      },
+      (error, options) => {
+        const { context, warnings, valid } = options || error.options
+        result.end = Date.now()
 
-      // All is well
-      if (valid && !warnings.length) return resolve(pass(result))
+        // All is well
+        if (valid && !warnings.length) return resolve(pass(result))
 
-      // Schema is not a valid schema
-      if (!valid) {
-        return resolve(fail({
-          ...result,
-          errorMessage: 'Not a valid OpenAPI schema.',
-        }))
+        // Schema is not a valid schema
+        if (!valid) {
+          return resolve(
+            fail({
+              ...result,
+              errorMessage: 'Not a valid OpenAPI schema.'
+            })
+          )
+        }
+
+        // Schema is valid, but warnings (lint failures) were present
+        // console.log({ context, warnings, valid })
+        const bad = toTestResult({
+          errorMessage: 'Schema is valid, but linting errors were present.',
+          stats: {
+            failures: warnings.length,
+            pending: 0,
+            passes: 0,
+            todo: 0,
+            start: result.start,
+            end: result.end
+          },
+          tests: warnings.map(
+            warningToTest(result.end - result.start, testPath)
+          ),
+          jestTestPath: testPath
+        })
+
+        // console.log(bad)
+
+        return resolve(bad)
       }
-
-      // Schema is valid, but warnings (lint failures) were present
-      // console.log({ context, warnings, valid })
-      const bad = toTestResult({
-        errorMessage: 'Schema is valid, but linting errors were present.',
-        stats: {
-          failures: warnings.length,
-          pending: 0,
-          passes: 0,
-          todo: 0,
-          start: result.start,
-          end: result.end,
-        },
-        tests: warnings.map(warningToTest(result.end - result.start, testPath)),
-        jestTestPath: testPath,
-      })
-
-      // console.log(bad)
-
-      return resolve(bad)
-    })
+    )
   })
 }
 
